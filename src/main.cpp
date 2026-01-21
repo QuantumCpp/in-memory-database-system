ad74304fa4582203e64ef90096357f7b6ab05285
QuantumCpp <jeremynobel16@gmail.com>
2026-01-21 16:59:14 -0500

    Irrelevante

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <string>
 #include <vector>
+#include <iostream>
+#include <filesystem> 
 
 //Comandos :
 //1. Listar archivos y carpetas de un directorio == list
@@ -7,7 +9,30 @@
 //  1.2 directorio raiz : ..
 //  1.3 directorio en especifico : ruta del directorio
 
+void list(std::vector<std::string>& argumentos){
+
+}
+
+void tokenizar(std::vector<std::string>& argumentos){
+
+}
+
+void Orquesta(std::vector<std::string>& argumentos){
+  if (argumentos[0] == "list"){
+    argumentos[0].clear();
+    list(argumentos);
+  }
+}
+
 int main(int argc, const char* argv[]){
-  std::vector<std::string> argumentos(argv,argv + argc);
+  if (argc > 1){
+    std::vector<std::string> argumentos(argv + 1,argv + argc);
+    Orquesta(argumentos);
+  }
+  else{
+    std::cout<<"PARAMETROS NO ESPECIFICADOS\n";
+    std::cout<<"LLAMDA [COMANDO] [ESPECIFICACIONES]\n";
+  }
+  return 0;
 }
 
