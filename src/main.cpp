8ca20f2c36faebfe44fbc2d8030fda1f36782d2b
QuantumCpp <jeremynobel16@gmail.com>
2026-01-20 22:28:02 -0500

    Omitible

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
-#include <iostream>
-#include <filesystem>
-#include <cstring>
+#include <string>
+#include <vector>
 
 //Comandos :
 //1. Listar archivos y carpetas de un directorio == list
@@ -9,12 +8,6 @@
 //  1.3 directorio en especifico : ruta del directorio
 
 int main(int argc, const char* argv[]){
-  int i = 0;
-
-  while(argv != nullptr){
-    if(std::strcmp(argv[i], "main")){
-      std::cout<<"comando llamada reconocido\n";
-    }
-  }
+  std::vector<std::string> argumentos(argv,argv + argc);
 }
 
