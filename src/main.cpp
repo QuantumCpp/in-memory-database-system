b8b1b86ea1f5523d90e00020d861255f6213a9d1
QuantumCpp <jeremynobel16@gmail.com>
2026-01-20 20:12:05 -0500

    Creacion de recepcion de comandos via consola

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
+#include <filesystem>
+#include <cstring>
 
-int main() {
-    int* p = new int(42);
-    delete p;
-    return *p; // ASAN explota aquí
+//Comandos :
+//1. Listar archivos y carpetas de un directorio == list
+//  1.1 directorio actual : .
+//  1.2 directorio raiz : ..
+//  1.3 directorio en especifico : ruta del directorio
+
+int main(int argc, const char* argv[]){
+  int i = 0;
+
+  while(argv != nullptr){
+    if(std::strcmp(argv[i], "main")){
+      std::cout<<"comando llamada reconocido\n";
+    }
+  }
 }
 
