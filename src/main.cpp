17ee7baf47bab7e3bd839073af1ec3a82bf76d87
QuantumCpp <jeremynobel16@gmail.com>
2026-01-21 22:37:50 -0500

    Add linear parser for commands, flags, options and positional arguments

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,81 @@
 #include <string>
+#include <unordered_map>
 #include <vector>
 #include <iostream>
 #include <filesystem> 
 
-//Comandos :
-//1. Listar archivos y carpetas de un directorio == list
-//  1.1 directorio actual : .
-//  1.2 directorio raiz : ..
-//  1.3 directorio en especifico : ruta del directorio
+//Comandos:
+//1.Listar archivos : Fecha de creacion - Nombre - Extension - Tamano 
 
-void list(std::vector<std::string>& argumentos){
+enum class TypeToken{
+  Command,
+  Flags,
+  OptionWithValue,
+  Positional
+};
+
+struct Token{
+  TypeToken type;
+  std::string name;
+  std::string value; //solo si es una OptionWithValue
+};
+
+bool StartWith(const std::string& arg, std::string start){
+  return !arg.empty() && arg.find(start) == 0;
+}
+
+void list(std::vector<std::string> arg){
 
 }
 
-void tokenizar(std::vector<std::string>& argumentos){
+int main(int argc, const char* argv[] ){
+  std::vector<std::string> arguments(argv + 1 , argv + argc);
+  std::unordered_map<std::string, bool> Options{
+    {"-f" , false},
+    {"-t" , false},
+    {"-e" , false},
+    {"-s" , false},
+    {"--extension" , true},
+    {"--size", true}
+  };
+  std::vector<Token> tokens;
 
-}
+  bool OptionOnlyPositional = false;
+  bool CommandFound = false;
 
-void Orquesta(std::vector<std::string>& argumentos){
-  if (argumentos[0] == "list"){
-    argumentos[0].clear();
-    list(argumentos);
-  }
-}
+  for(size_t i = 0 ; i < arguments.size() ; i++){
+    const std::string& args = arguments[i];
+    if(args == "--"){
+      OptionOnlyPositional = true;
+      continue;
+    }
+    
+    if (!OptionOnlyPositional && !StartWith(args, "-") && !CommandFound){
+      tokens.emplace_back(Token{TypeToken::Command, args, ""});
+      CommandFound = true;
+      continue;
+    }
+    
+    if(!OptionOnlyPositional && StartWith(args, "-")){
+      if(StartWith(args,"--")){
+        auto pos = args.find("="); //Retorna Npos si no hay valor = en el argumento
+        if (pos != std::string::npos){
+          tokens.emplace_back(Token{TypeToken::OptionWithValue, args.substr(0, pos), args.substr(pos + 1)});
 
-int main(int argc, const char* argv[]){
-  if (argc > 1){
-    std::vector<std::string> argumentos(argv + 1,argv + argc);
-    Orquesta(argumentos);
+        }
+        else{
+          tokens.emplace_back(Token{TypeToken::OptionWithValue, args, ""});
+        }
+        continue;
+      }
+      tokens.emplace_back(Token{TypeToken::Flags, args, ""});
+      continue;
+    }
+
+    if ((OptionOnlyPositional || !StartWith(args,"-")) && CommandFound){
+      tokens.emplace_back(Token{TypeToken::Positional, args, ""});
+      continue;
+    }
   }
-  else{
-    std::cout<<"PARAMETROS NO ESPECIFICADOS\n";
-    std::cout<<"LLAMDA [COMANDO] [ESPECIFICACIONES]\n";
-  }
-  return 0;
 }
 
